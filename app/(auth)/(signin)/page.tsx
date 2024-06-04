bf8192d917fc6b18e3742ea16251dee4f770914f
John Paul <iamjohnpaulr5@gmail.com>
2024-06-04 23:10:21 +0530

    refactor: Update testimonial quote and author in page.tsx

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -41,11 +41,11 @@ export default function AuthenticationPage() {
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
+              &ldquo;This  has sasitesved me countless hours of work and
+              helped me with excellent notes to my transcripts and youtube  links faster than ever
               before.&rdquo;
             </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <footer className="text-sm">John Paul</footer>
           </blockquote>
         </div>
       </div>
